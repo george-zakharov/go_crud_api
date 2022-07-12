60796f4b6b85dbda34599d123d17dc81c605d5a4
George Zakharov <george_zakharov@mail.ru>
2022-07-13 01:13:05 +0300

    Add base operations

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/george-zakharov/go_crud_api
 
 go 1.17
 
-require github.com/gorilla/mux v1.8.0 // indirect
+require github.com/gorilla/mux v1.8.0
--- main.go
+++ main.go
@@ -1,1 +1,37 @@
 package main
+
+import (
+	"fmt"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+)
+
+type Movie struct {
+	ID       string    `json:"id"`
+	Isbn     string    `json:"isbn"`
+	Title    string    `json:"title"`
+	Director *Director `json:"director"`
+}
+
+type Director struct {
+	Firstname string `json:"firstname"`
+	Lastname  string `json:"lastname"`
+}
+
+var movies []Movie
+
+func main() {
+	movies = append(movies, Movie{ID: "1", Isbn: "123456", Title: "Movie One", Director: &Director{Firstname: "John", Lastname: "Doe"}})
+	movies = append(movies, Movie{ID: "2", Isbn: "678345", Title: "Movie Two", Director: &Director{Firstname: "Mary", Lastname: "Doe"}})
+
+	r := mux.NewRouter()
+	r.HandleFunc("/movies", getMovies).Methods("GET")
+	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
+	r.HandleFunc("/movies", createMovie).Methods("POST")
+	r.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
+	r.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
+
+	fmt.Printf("Starting port at 8000\n")
+	log.Fatal(http.ListenAndServe("8000", r))
+}
